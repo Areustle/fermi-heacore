36369360492c7fb83b90305b4c122059666f9721
areustle <areustledev@gmail.com>
2017-07-07 16:56:38 -0400

    avoid x86_64 directory in prefix

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ export CFLAGS='-I${PREFIX}/include -O2 -Wall --pedantic -Wno-comment -Wno-long-l
 export CXXFLAGS='-I${PREFIX}/include -O2 -Wall --pedantic -Wno-comment -Wno-long-long -g  -ffloat-store -fPIC'
 export CPPFLAGS="-I${PREFIX}/include"
 export LDFLAGS="-L${PREFIX}/lib"
-./configure --prefix=$PREFIX
-./hmake 
-./hmake install
+./configure --prefix=${PREFIX} --exec_prefix=${PREFIX}
+./hmake HD_EXEC_PFX=${PREFIX} HD_TOP_EXEC_PFX=${PREFIX}
+./hmake install HD_EXEC_PFX=${PREFIX} HD_TOP_EXEC_PFX=${PREFIX}
