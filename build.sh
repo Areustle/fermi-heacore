9fd0d150dbfa491776063e26ad2130bb80862722
areustle <areustledev@gmail.com>
2017-07-14 23:35:13 -0400

    link against anaconda libs usink -rpath

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ export CFLAGS='-I${PREFIX}/include -O2 -Wall --pedantic -Wno-comment -Wno-long-l
 export CXXFLAGS='-I${PREFIX}/include -O2 -Wall --pedantic -Wno-comment -Wno-long-long -g  -ffloat-store -fPIC'
 export CPPFLAGS="-I${PREFIX}/include"
 export LDFLAGS="-L${PREFIX}/lib"
-./configure --prefix=${PREFIX} --exec_prefix=${PREFIX}
+./configure --prefix=${PREFIX} --exec_prefix=${PREFIX} LDFLAGS="-Wl,-rpath,${PREFIX}/lib -L${PREFIX}/lib"
 ./hmake HD_EXEC_PFX=${PREFIX} HD_TOP_EXEC_PFX=${PREFIX}
 ./hmake install HD_EXEC_PFX=${PREFIX} HD_TOP_EXEC_PFX=${PREFIX}
